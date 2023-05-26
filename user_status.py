f2fa8c6342693e1d5205398188bfe5d6b7ac6fec
root <showell30@yahoo.com>
2023-05-26 19:12:18 +0000

    progress

--- user_status.py
+++ user_status.py
@@ -8,10 +8,11 @@ def send(content):
     request = {
         "type": "stream",
         "to": "bot testing",
-        "topic": "steve bot",
+        "topic": "status changes",
         "content": content,
     }
     result = client.send_message(request)
+    print("sending", content)
 
 def handle_event(event):
     if event["type"] != "user_status":
@@ -19,7 +20,9 @@ def handle_event(event):
     user_id = event["user_id"]
     user = client.get_user_by_id(user_id)["user"]
     user_name = user["full_name"]
-    send(f"{user_name} changed their status")
+    status_text = event["status_text"]
+    send(f"{user_name} changed their status to {status_text}")
     print("message sent")
 
+print("waiting for changes")
 client.call_on_each_event(handle_event)
