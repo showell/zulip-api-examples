1a62f3f317736074093de038bd05d1df1595b23e
Brijmohan Siyag <brijsiyag@gmail.com>
2023-06-04 04:58:27 +0530

    add: Generalized version of topic_links bot.

--- topic_links_czo.py
+++ topic_links_czo.py
@@ -1,40 +1,45 @@
+# This file is specially for chat.zulip.org
+
 import re
 import zulip
 
+# CONFIG: change these for your installation
+STREAM_FOR_NOTIFICATIONS = "bot testing"
+TOPIC_NAME_FOR_NOTIFICATIONS = "topic links"
+
 client = zulip.Client(config_file="~/zuliprc")
 
-def send(content,topic,stream):
+def send(content):
     # https://zulip.com/api/send-message
     request = {
         "type": "stream",
-        "to": stream,
-        "topic": topic,
+        "to": STREAM_FOR_NOTIFICATIONS,
+        "topic": TOPIC_NAME_FOR_NOTIFICATIONS,
         "content": content,
     }
-    client.send_message(request)
-    print(f"sent->{content}")
+    print ("sending", content)
+    result = client.send_message(request)
 
 def handle_message(msg):
     if msg["type"] != "stream":
-        print("Ignoring DM...")
-        return
-    if msg["sender_email"] == client.email:
+        print("ignoring DM")
         return
 
-    TOPIC_LINK_RE = r"(\#\*\*(.*)>(.*)\*\*)"
+    TOPIC_LINK_RE = "(\#\*\*.*?>.*?\*\*)"
 
     content = msg["content"]
+    if "#**" in content and not "links to" in content:
+        stream = msg["display_recipient"]
+        topic = msg["subject"]
 
-    stream = msg["display_recipient"]
-    topic = msg["subject"]
+        if topic == TOPIC_NAME_FOR_NOTIFICATIONS:
+            return
 
-    from_topic_link = f"#**{stream}>{topic}**"
+        from_topic_link = f"#**{stream}>{topic}**"
 
-    for tagged_topic_link, tagged_stream, tagged_topic in re.findall(TOPIC_LINK_RE, content):
-        if tagged_topic_link == from_topic_link:
-            continue
-        msg = f"This topic was mentioned in {from_topic_link}"
-        send(msg,tagged_topic,tagged_stream)
+        for to_topic_link in re.findall(TOPIC_LINK_RE, content):
+            new_content = f"{from_topic_link} links to {to_topic_link}"
+            send(new_content)
 
 def watch_messages():
     print("Watching for messages...")
